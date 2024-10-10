73442832565dcadd0464461de8913567e16a5950
Saima <mukadam.saima@gmail.com>
2024-10-10 01:57:51 -0700

    typo: changed game name

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Saima's Game";
+const gameName = "Saima's Game"; // changed this line
 document.title = gameName;
 
 const header = document.createElement("h1");
