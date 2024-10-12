2d59b7aba6f13244d1c3cbe386b68d1eced507c1
Saima <mukadam.saima@gmail.com>
2024-10-11 23:48:42 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Saima's Awesome Cat Petting Game"; // changed this line
+const gameName = "Saima's Awesome Cat Petting Game 🐈"; // changed this line
 document.title = gameName;
 
 const header = document.createElement("h1"); // put "button"
@@ -19,3 +19,15 @@ const pettingButton = "🫳";
 const button = document.createElement("button"); // put "button"
 button.innerHTML = pettingButton;
 app.append(button);
+
+// Step 2: Clicking increases a counter
+// div element: "# pets!"
+let counter: number = 0; // initializing counter w/ type annotation
+const counterDisplay = document.createElement("p"); // create paragraph to display count
+counterDisplay.innerHTML = `${counter} pets!`;
+app.append(counterDisplay);
+
+button.addEventListener("click", () => {
+    counter++; // incrementing counter each time u click
+    counterDisplay.innerHTML = `${counter} pets!`; // update the display
+});
