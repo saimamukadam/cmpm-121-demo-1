af10cd6acef6c9e23799c0a3f5beb77701a17abd
Saima <mukadam.saima@gmail.com>
2024-10-11 21:04:32 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Saima's Game"; // changed this line
+const gameName = "Saima's Awesome Cat Petting Game"; // changed this line
 document.title = gameName;
 
-const header = document.createElement("h1");
+const header = document.createElement("h1"); // put "button"
 header.innerHTML = gameName;
 app.append(header);
+
+// MY CODE HERE
+
+// game premise: pet cat by pressing button 
+// emoji to display: petting hand 🫳, cat 🐈, fish cake 🍥, yarn 🧶
+
+// Step 1: A button you can click
+const pettingButton = "🫳";
+const button = document.createElement("button"); // put "button"
+button.innerHTML = pettingButton;
+app.append(button);
